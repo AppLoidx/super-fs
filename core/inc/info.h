b1108dfc1c24ac12d7a9059ce8aa5130cbd95b7c
Lilith <apploidyakutsk@gmail.com>
2021-02-21 21:19:36 +0300

    super structure update

---
+++ core/inc/info.h
@@ -0,0 +1,6 @@
+#ifndef SUPER_FS_INFO_CORE_H
+#define SUPER_FS_INFO_CORE_H
+
+void printInfo(FILE* file);
+
+#endif
---
+++ core/src/info.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include "../inc/info.h"
+
+void printfInfo(FILE * file) {
+  fputs("Printing info about FS", file);
+}
+
